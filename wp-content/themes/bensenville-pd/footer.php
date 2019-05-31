39babcc4ecbf86675485bebae847d9f25b924630
manish <manish@kgntechbologies.com>
2019-05-31 14:34:30 +0530

    Dynamic footer and home page

--- wp-content/themes/bensenville-pd/footer.php
+++ wp-content/themes/bensenville-pd/footer.php
@@ -13,6 +13,13 @@
 
 </div><!-- #content -->
 
+<?php 
+	$logo_text = get_field('logo_text','option');
+	$address = get_field('address','option');
+	$phone_number = get_field('phone_number','option');
+	$newsletter_shortcode = get_field('newsletter_shortcode','option');
+	
+?>
 <footer id="colophon" class="site-footer">
 	<div class="footer">
 		<div class="container">
@@ -20,29 +27,24 @@
 				<div class="col-md-2">
 					<div class="footer-logo">
 						<a href="#" alt=""><img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" alt="footer-logo"></a>
-						<p class="text-right">“It’s all Around You”</p>
+						<?php if(!empty($logo_text)){?><p class="text-right"><?php echo $logo_text;?></p><?php } ?>
 					</div>
 				</div>
 				<div class="col-md-2">
 					<div class="footer-contact-box">
-						<p>1000 W. Wood St. Bensenville Illinois 60106</p>
-						<a href="tel:630-766-7015">630-766-7015</a>
+						<?php if(!empty($address)){?><p><?php echo $address;?></p><?php } ?>
+						<?php if(!empty($phone_number)){?><a href="tel:<?php echo $phone_number;?>"><?php echo $phone_number;?></a><?php } ?>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="newsletter">
-						<h5>Newsletter Signup</h5>
-						<p>Sign up for our email newsletter and recieve monthly updates on programs, events and news.</p>
-						<form class="newsletter-form">
-							<input type="email" name="" class="form-control" placeholder="Email Address:">
-							<button class="btn btn-yellow">Signup</button>
-						</form>
+						<?php echo do_shortcode($newsletter_shortcode);?>
 					</div>
 				</div>
 				<div class="col-md-2">
 					<div id="google_translate_element"></div>
 
-					<p class="copyright">&copy; 2019 Bensenville Park District, All Rights Reserved</p>
+					<p class="copyright">&copy; <?php echo date('Y');?> Bensenville Park District, All Rights Reserved</p>
 				</div>
 			</div>
 		</div>
---
+++ wp-content/themes/bensenville-pd/template-parts/content-sidebar.php
@@ -0,0 +1,39 @@
+<?php
+	$winter_spring_title = get_field('winter_spring_title','option');
+	$winter_spring_description = get_field('winter_spring_description','option');
+	$winter_spring_background_image = get_field('winter_spring_background_image','option');
+	$winter_spring_link_section = get_field('winter_spring_link_section','option');	
+	?>
+
+<?php if(is_front_page()) {?>
+<div class="box box-bg-gradient clearfix">
+	<?php if(!empty($winter_spring_background_image)){?>
+		<img src="<?php echo $winter_spring_background_image['url']?>" alt="">
+	<?php } ?>
+	<?php if(!empty($winter_spring_title) || !empty($winter_spring_description)){?>
+	<div class="box-content">
+		<?php if(!empty($winter_spring_title)){?><h5 class="box-title"><?php echo $winter_spring_title;?></h5><?php } ?>
+		<?php if(!empty($winter_spring_description)){?><p><?php echo $winter_spring_description;?></p><?php } ?>
+		<?php
+		if(!empty($winter_spring_link_section)){
+			foreach ($winter_spring_link_section as $winter_spring_link) {
+			$winter_spring_link_title = $winter_spring_link['winter_spring_link_title'];
+			$winter_spring_link_type = $winter_spring_link['winter_spring_link_type'];
+			$winter_spring_internal_link = $winter_spring_link['winter_spring_internal_link'];
+			$winter_spring_external_link = $winter_spring_link['winter_spring_external_link'];
+		?>
+		<?php if(!empty($winter_spring_link_title) && $winter_spring_link_type=='Internal'){?>
+			<a href="<?php echo $winter_spring_internal_link;?>" class="box-link"><i class="fa fa-angle-right"></i> <?php echo $winter_spring_link_title;?></a>
+		<?php } ?>
+		<?php if(!empty($winter_spring_link_title) && $winter_spring_link_type=='External'){?>
+			<a href="<?php echo $winter_spring_external_link;?>" class="box-link" target="_blank"><i class="fa fa-angle-right"></i> <?php echo $winter_spring_link_title;?></a>
+		<?php } ?>
+		<?php 	}
+		}
+		?>
+	</div>
+	<?php } ?>
+</div>
+<?php }else{
+echo "fsdf";
+} ?>
