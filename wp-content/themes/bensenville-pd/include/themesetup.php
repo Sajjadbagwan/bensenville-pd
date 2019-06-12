34fff2362990ad2d8c20b85876e2ce622f2e3826
manish <manish@kgntechbologies.com>
2019-06-12 11:26:59 +0530

    change name

--- wp-content/themes/bensenville-pd/include/themesetup.php
+++ wp-content/themes/bensenville-pd/include/themesetup.php
@@ -129,7 +129,7 @@ if(function_exists('acf_add_options_page'))
 {
     acf_add_options_page (array(
         'page_title'=>'General Setting',
-        'menu_title'=>'General Setting',
+        'menu_title'=>'General Settings',
         'menu_slug'=>'theme-general-settings',
         'capability'=>'edit_posts',
         'redirect' => false
